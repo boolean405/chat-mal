6a7619c970ebf80e008a317db53e0c718307886b
Boolean <boolean405@gmail.com>
2025-07-18 04:00:17 +0700

    latest one

--- server/socket/fetchAll.js
+++ server/socket/fetchAll.js
@@ -1,5 +1,6 @@
 import ChatDB from "../models/chat.js";
 import MessageDB from "../models/message.js";
+import Redis from "../config/redisClient.js"; // make sure Redis is imported
 import { REDIS_ONLINE_USERS_KEY } from "../constants/index.js";
 
 export default async function fetchAll(io, socket) {
@@ -23,6 +24,8 @@ export default async function fetchAll(io, socket) {
         const messages = await MessageDB.find({
           chat: chat._id,
           ...(deletedAt && { createdAt: { $gt: deletedAt } }),
+          status: "sent", // only messages that are still 'sent'
+          sender: { $ne: userId }, // not sent by current user
         })
           .sort({ createdAt: -1 })
           .limit(unreadInfo.count)
@@ -38,36 +41,40 @@ export default async function fetchAll(io, socket) {
             ],
           });
 
-        messages.reverse().forEach((message) => {
-          socket.emit("new-message", { message });
-        });
+        for (const msg of messages.reverse()) {
+          // Update status to delivered
+          await MessageDB.updateOne(
+            { _id: msg._id },
+            { $set: { status: "delivered" } }
+          );
+
+          // Emit to the current (receiver) user
+          const updatedMessage = { ...msg.toObject(), status: "delivered" };
+          socket.emit("receive-message", { message: updatedMessage });
+
+          socket.emit("new-message", {
+            chatId: msg.chat._id.toString(),
+            message: updatedMessage,
+          });
+
+          // Notify sender if they're online
+          const senderId = msg.sender._id.toString();
+          const senderSocketId = await Redis.hGet(
+            REDIS_ONLINE_USERS_KEY,
+            senderId
+          );
+          if (senderSocketId) {
+            io.to(senderSocketId).emit("new-message", {
+              chatId: msg.chat._id.toString(),
+              message: updatedMessage,
+            });
+          }
+        }
       }
-      // Emit the chat (even if no unread messages)
+
+      // Always emit the chat itself
       socket.emit("new-chat", { chat });
     }
-
-    // Mark as delivered when online
-    // for (const chat of chats) {
-    //   const messages = await MessageDB.find({
-    //     chat: chat._id,
-    //     sender: { $ne: userId }, // messages not sent by this user
-    //     createdAt: { $gt: socket.user.lastOnlineAt || new Date(0) }, // new messages
-    //   });
-
-    //   for (const message of messages) {
-    //     // Notify the sender if online
-    //     const senderSocketId = await Redis.hGet(
-    //       REDIS_ONLINE_USERS_KEY,
-    //       message.sender.toString()
-    //     );
-    //     if (senderSocketId) {
-    //       io.to(senderSocketId).emit("message-delivered", {
-    //         messageId: message._id,
-    //         // deliveredTo: userId,
-    //       });
-    //     }
-    //   }
-    // }
   } catch (err) {
     console.error("❌ Failed to resend messages:", err.message);
     socket.emit("error", { message: "Could not sync missed messages." });
