1770d96f152a6cdef863fafa37a50969f84243c9
Boolean <boolean405@gmail.com>
2025-08-28 06:47:47 +0700

    fix website

--- mobile/constants/index.ts
+++ mobile/constants/index.ts
@@ -4,7 +4,7 @@ import Constants from "expo-constants";
 export const APP_NAME = "Chat Mal";
 export const APP_TAGLINE = "Explore the World";
 export const CS_EMAIL = "chatmalapp@gmail.com";
-export const WEBSITE_URL = "https://chatmal.com";
+export const WEBSITE_URL = "https://chatmal.vercel.app";
 
 // Envionment variables
 export const SERVER_URL = process.env.EXPO_PUBLIC_SERVER_URL;
