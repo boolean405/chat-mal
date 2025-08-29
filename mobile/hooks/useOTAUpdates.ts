a7bbe97cb9b763795d2b9d4579b3c0a68348121a
Boolean <boolean405@gmail.com>
2025-08-30 06:41:10 +0700

    fix update

---
+++ mobile/hooks/useOTAUpdates.ts
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import * as Updates from "expo-updates";
+import { Alert } from "react-native";
+
+export default function useOTAUpdates() {
+  const { isUpdateAvailable, isUpdatePending } = Updates.useUpdates();
+
+  useEffect(() => {
+    if (isUpdateAvailable) {
+      Updates.fetchUpdateAsync();
+    }
+  }, [isUpdateAvailable]);
+
+  useEffect(() => {
+    if (isUpdatePending) {
+      Alert.alert(
+        "Update available",
+        "A new version has been downloaded. Apply to restart.",
+        [
+          {
+            text: "Apply",
+            onPress: () => Updates.reloadAsync(),
+          },
+        ]
+      );
+    }
+  }, [isUpdatePending]);
+}
